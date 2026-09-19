41b315807fb83e51c23291062a0b2e0dc04aaeb4
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-09-20 01:17:34 +0800

    some adjustment

--- backend/listings/management/commands/seed_data.py
+++ backend/listings/management/commands/seed_data.py
@@ -6,26 +6,26 @@ from listings.models import BoardingHouse, Faculty, RoadSegment
 # NOTE: These are placeholder coordinates for demo/testing purposes only.
 
 FACULTIES = [
-    {"name": "Teknik Informatika", "address": "Jl. Unima, Tataaran Satu, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "lng": 1.2645261151216165, "lat": 124.88770287316223},
-    {"name": "Pendidikan Teknik Informatika", "address": "Jl. Unima, Tataaran Satu, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "lng": 1.2657802515003769, "lat": 124.88718718298111},
-    {"name": "Teknik Mesin", "address": "Tataaran Satu, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "lng": 1.2668400535311486, "lat": 124.88616152977424},
-    {"name": "Teknik Arsitektur", "address": "Tataaran Satu, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "lng": 1.2660404346465146, "lat": 124.88805031842583},
+    {"name": "Teknik Informatika", "address": "Jl. Unima, Tataaran Satu, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "lng": 124.88770287316223, "lat": 1.2645261151216165},
+    {"name": "Pendidikan Teknik Informatika", "address": "Jl. Unima, Tataaran Satu, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "lng": 124.88718718298111, "lat": 1.2657802515003769},
+    {"name": "Teknik Mesin", "address": "Tataaran Satu, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "lng": 124.88616152977424, "lat": 1.2668400535311486},
+    {"name": "Teknik Arsitektur", "address": "Tataaran Satu, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "lng": 124.88805031842583, "lat": 1.2660404346465146},
 ]
 
 BOARDING_HOUSES = [
     {
         "name": "Kost Vegaly", "price_min": 350000, "price_max": 500000, "room_type": "campur",
-        "facilities": ["wifi", "kamar_mandi_dalam"], "lng": 1.2681638000846318, "lat": 124.87652951050738,
+        "facilities": ["wifi", "kamar_mandi_dalam"], "lng": 124.87652951050738, "lat": 1.2681638000846318,
         "address": "Tataaran Patar, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "contact": "0812-0000-0001",
     },
     {
         "name": "Sakura House", "price_min": 500000, "price_max": 800000, "room_type": "campur",
-        "facilities": ["wifi", "parkir", "kamar_mandi_dalam"], "lng": 1.2664309429429765, "lat": 124.87498434847735,
+        "facilities": ["wifi", "parkir", "kamar_mandi_dalam"], "lng": 124.87498434847735, "lat": 1.2664309429429765,
         "address": "Tataaran Patar, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "contact": "0812-0000-0002",
     },
     {
         "name": "Kost Gloria", "price_min": 350000, "price_max": 500000, "room_type": "campur",
-        "facilities": ["wifi", "ac", "kamar_mandi_dalam", "laundry"], "lng": 1.26712905083638, "lat": 124.87574270106187,
+        "facilities": ["wifi", "ac", "kamar_mandi_dalam", "laundry"], "lng": 124.87574270106187, "lat": 1.26712905083638,
         "address": "perum unima Kel maesa, Tataaran Patar, Kec. Tondano Sel., Kabupaten Minahasa, Sulawesi Utara", "contact": "0812-0000-0003",
     },
 ]
